c7f58c8e2f72010a12f401a3e3f05a0c9b4a4589
Nashrul Saefullah <nashrulsaefullah@gmail.com>
2020-10-26 19:17:14 +0700

    add button into body and cut code title and paste to title file

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:plant_app/constant.dart';
 import 'package:plant_app/screens/home/components/header_with_searchbox.dart';
+import 'package:plant_app/screens/home/components/title_with_more_bbtn.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -10,73 +10,10 @@ class Body extends StatelessWidget {
       child: Column(
         children: [
           HeaderWithSearchBox(size: size),
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: kDefaultPadding,
-            ),
-            child: Row(
-              children: [
-                TitleWithCustomUnderLine(
-                  text: "Recomended",
-                ),
-                Spacer(),
-                FlatButton(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  onPressed: () {},
-                  color: kPrimaryColor,
-                  child: Text(
-                    "More",
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ],
-            ),
+          TitleWithMoreBtn(
+            title: "Recomended",
+            press: () {},
           ),
-        ],
-      ),
-    );
-  }
-}
-
-class TitleWithCustomUnderLine extends StatelessWidget {
-  const TitleWithCustomUnderLine({
-    Key key,
-    this.text,
-  }) : super(key: key);
-
-  final String text;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 24,
-      child: Stack(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(
-              left: kDefaultPadding / 4,
-            ),
-            child: Text(
-              text,
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          Positioned(
-              bottom: 0,
-              right: 0,
-              left: 0,
-              child: Container(
-                margin: EdgeInsets.only(right: kDefaultPadding / 4),
-                height: 7,
-                color: kPrimaryColor.withOpacity(0.2),
-              ))
         ],
       ),
     );
