4ac22a485aded463a852d5ea428d56a51222f8ad
Nashrul Saefullah <nashrulsaefullah@gmail.com>
2020-10-26 13:56:14 +0700

    add color to use in app

---
+++ lib/constant.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Colors.green;
+const kTextColor = Color(0xFF3C4046);
+const kBackgroundColor = Colors.white;
+
+const double kDefaultPadding = 20.0;
