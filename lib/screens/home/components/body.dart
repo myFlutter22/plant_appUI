ef54f5898d2f9b8d003830091b2ab6da7b52f716
Nashrul Saefullah <nashrulsaefullah@gmail.com>
2020-10-26 16:03:16 +0700

    add text and logo

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -6,65 +6,99 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Column(
-      children: [
-        Container(
-          height: size.height * 0.2,
-          child: Stack(
-            children: [
-              Container(
-                height: size.height * 0.2 - 27,
-                decoration: BoxDecoration(
-                  color: kPrimaryColor,
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(36),
-                    bottomRight: Radius.circular(36),
-                  ),
-                ),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          HeaderWithSearchBox(size: size),
+        ],
+      ),
+    );
+  }
+}
+
+class HeaderWithSearchBox extends StatelessWidget {
+  const HeaderWithSearchBox({
+    Key key,
+    @required this.size,
+  }) : super(key: key);
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: size.height * 0.2,
+      child: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.only(
+              left: kDefaultPadding,
+              right: kDefaultPadding,
+              bottom: kDefaultPadding + 30,
+            ),
+            height: size.height * 0.2 - 27,
+            decoration: BoxDecoration(
+              color: kPrimaryColor,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(36),
+                bottomRight: Radius.circular(36),
               ),
-              Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: Container(
-                  margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                  padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                  alignment: Alignment.center,
-                  height: 54,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                      BoxShadow(
-                          offset: Offset(0, 10),
-                          blurRadius: 50,
-                          color: kPrimaryColor.withOpacity(0.23)),
-                    ],
-                  ),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: TextField(
-                          onChanged: (value) {},
-                          decoration: InputDecoration(
-                            hintText: "Search",
-                            hintStyle: TextStyle(
-                              color: kPrimaryColor.withOpacity(0.5),
-                            ),
-                            enabledBorder: InputBorder.none,
-                            focusedBorder: InputBorder.none,
-                          ),
-                        ),
+            ),
+            child: Row(
+              children: [
+                Text(
+                  "Hi UIshoppy!",
+                  style: Theme.of(context).textTheme.headline5.copyWith(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
                       ),
-                      SvgPicture.asset("assets/icons/search.svg"),
-                    ],
-                  ),
                 ),
-              ),
-            ],
+                Spacer(),
+                Image.asset("assets/images/logo.png")
+              ],
+            ),
           ),
-        ),
-      ],
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              alignment: Alignment.center,
+              height: 54,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                      offset: Offset(0, 10),
+                      blurRadius: 50,
+                      color: kPrimaryColor.withOpacity(0.23)),
+                ],
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      onChanged: (value) {},
+                      decoration: InputDecoration(
+                        hintText: "Search",
+                        hintStyle: TextStyle(
+                          color: kPrimaryColor.withOpacity(0.5),
+                        ),
+                        enabledBorder: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  SvgPicture.asset("assets/icons/search.svg"),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
